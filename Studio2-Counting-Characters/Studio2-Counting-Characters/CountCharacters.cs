488be6b5a5092cb81416ecedccc28920ccff9cf6
Sam <samtpomarico@gmail.com>
2020-10-01 20:17:43 -0500

    First commit. Studio and bonus complete

---
+++ Studio2-Counting-Characters/Studio2-Counting-Characters/CountCharacters.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Studio2_Counting_Characters
+{
+    class CountCharacters
+    {
+        public static void countCharacters(string sentence)
+        {
+            sentence = sentence.ToLower();
+            char[] charactersInString = sentence.ToCharArray();
+            Dictionary<char, int> output = new Dictionary<char, int>();
+            char[] accepted = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            foreach (char i in charactersInString)
+            {
+                if (accepted.Contains(i))
+                {
+                    if (output.ContainsKey(i))
+                    {
+                        output[i] += 1;
+                    }
+                    else
+                    {
+                        output.Add(i, 1);
+                    }
+                } else
+                {
+                    continue;
+                }
+            }
+            foreach (KeyValuePair<char, int> letter in output)
+            {
+                Console.WriteLine(letter.Key + ": " + letter.Value);
+            }
+        }
+    }
+}
---
+++ Studio2-Counting-Characters/Studio2-Counting-Characters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Studio2_Counting_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string someString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+
+            Console.WriteLine("Enter a string to count characters for: ");
+            string input = Console.ReadLine();
+            CountCharacters.countCharacters(input);
+        }
+    }
+}
